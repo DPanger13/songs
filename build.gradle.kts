8b4a02fe7a4a22e9e8ec317b8607cb25ae33a97d
Derek <dpanger13@gmail.com>
2023-02-24 21:17:03 -0600

    Add template

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("multiplatform") version "1.7.21"
+}
+
+group = "com.dpanger"
+version = "0.0"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+    }
+}
---
+++ src/jvmMain/kotlin/com/dpanger/songs/JvmEmptyClass.kt
@@ -0,0 +1,4 @@
+package com.dpanger.songs
+
+class JvmEmptyClass {
+}
