9367caa266964a7bc4a4a06879b54cb7ad0ce94e
Derek <dpanger13@gmail.com>
2023-02-25 22:54:53 -0600

    Convert JVM target to Android

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,18 @@
 plugins {
     kotlin("multiplatform") version "1.7.21"
+    id("com.android.library")
 }
 
 group = "com.dpanger"
 version = "0.0"
 
 repositories {
+    google()
     mavenCentral()
 }
 
 kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-        withJava()
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
-    }
+    android()
     sourceSets {
         val commonMain by getting
         val commonTest by getting {
@@ -26,7 +20,20 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
-        val jvmMain by getting
-        val jvmTest by getting
+        val androidMain by getting
+        val androidTest by getting
+    }
+}
+
+android {
+    compileSdkVersion(31)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdkVersion(24)
+        targetSdkVersion(31)
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
